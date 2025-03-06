bb4d5f1f34f97918f14ccd546f7f664d82a5f473
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-03-06 18:24:06 +0800

    优化数据包处理逻辑，简化IP层检查并改进日志输出格式

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -9,26 +9,24 @@ logger = setup_logging()
 
 
 def packet_handler(packet):
-    if packet.haslayer(IP):
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        proto = packet[IP].proto
-        logger.info(f"捕获到数据包: {src_ip} -> {dst_ip} 协议类型: {proto}")
+    if not packet.haslayer(IP):
+        return
 
-        if packet.haslayer(ICMP):
-            icmp_type = packet[ICMP].type
-            icmp_code = packet[ICMP].code
-            logger.info(f"ICMP类型: {icmp_type}, 代码: {icmp_code}")
-        elif packet.haslayer(TCP):
-            src_port = packet[TCP].sport
-            dst_port = packet[TCP].dport
-            logger.info(f"TCP端口: {src_port} -> {dst_port}")
+    src_ip = packet[IP].src
+    dst_ip = packet[IP].dst
+    if packet.haslayer(ICMP):
+        icmp_type = packet[ICMP].type
+        logger.info(f"{src_ip} -> {dst_ip} ICMP: {icmp_type}")
+    elif packet.haslayer(TCP):
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+        logger.info(f"{src_ip}:{src_port} -> {dst_ip}:{dst_port} TCP")
 
 
 # 抓包逻辑
 try:
     logger.info("开始抓包，按Ctrl+C停止...")
-    sniff(filter="tcp", prn=packet_handler, store=0)
+    sniff(prn=packet_handler, store=0)
 except KeyboardInterrupt:
     logger.info("抓包结束，退出程序...")
     sys.exit(0)
