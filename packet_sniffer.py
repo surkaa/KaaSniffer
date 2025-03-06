0dfa833fa3f2ed928ad1672f17ae1bda7e209638
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-03-06 17:45:44 +0800

    添加日志功能以替代打印输出，增强数据包捕获信息的记录

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,4 +1,8 @@
+import logging
 import sys
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 from scapy.layers.inet import IP, ICMP
 from scapy.sendrecv import sniff
@@ -10,19 +14,20 @@ def packet_handler(packet):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
         proto = packet[IP].proto
-        print(f"捕获到数据包: {src_ip} -> {dst_ip} 协议类型: {proto}")
+        logger.info(f"捕获到数据包: {src_ip} -> {dst_ip} 协议类型: {proto}")
 
         # 如果是ICMP包（如ping），提取更多信息
         if packet.haslayer(ICMP):
             icmp_type = packet[ICMP].type
             icmp_code = packet[ICMP].code
-            print(f"  ICMP类型: {icmp_type}, 代码: {icmp_code}\n")
+            logger.info(f"ICMP类型: {icmp_type}, 代码: {icmp_code}")
 
 
 # 抓包逻辑
 try:
-    print("开始抓包，按Ctrl+C停止...")
+    # print("开始抓包，按Ctrl+C停止...")
+    logger.info("开始抓包，按Ctrl+C停止...")
     sniff(filter="icmp", prn=packet_handler, store=0)  # 只抓ICMP包，不存储
 except KeyboardInterrupt:
-    print("\n抓包已停止")
+    logger.info("抓包结束，退出程序...")
     sys.exit(0)
